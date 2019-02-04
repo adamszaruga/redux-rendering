c39e91c15096434c065f834222989aab61dacbd7
Adam Szaruga <adamszaruga@gmail.com>
2019-02-03 22:54:44 -0500

    initial commit

---
+++ circles/reducer.js
@@ -0,0 +1,27 @@
+const initialState = [
+    {
+        radius: 50,
+        color: "#FF00FF"
+    },
+    {
+        radius: 30,
+        color: "#FF99AA"
+    },
+    {
+        radius: 60,
+        color: "#0000FF"
+    },
+    {
+        radius: 10,
+        color: "#000000"
+    },
+];
+
+const reducer = (state = initialState, action) => {
+    // Handle actions here - make sure you don't mutate the state!
+    const { type } = action;
+
+    // ACTION: Add a random circle
+
+    return state;
+}
